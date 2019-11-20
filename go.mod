6ab97679e3c64166a7bb3eae9f237291ec29dd23
Jules Mikeal <julesmichaelgiovanni@gmail.com>
2019-11-20 13:40:55 +0400

    updaterTestSuite

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go.lsl.digital/updater
 
 go 1.13
+
+require (
+	github.com/stretchr/testify v1.4.0
+	gopkg.in/go-playground/assert.v1 v1.2.1
+)
--- updater_test.go
+++ updater_test.go
@@ -1,1 +1,29 @@
 package updater_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+// UpdaterTestSuite is the main test suite for package updater
+type UpdaterTestSuite struct {
+	suite.Suite
+}
+
+// SetupTest setups the test suite for future tests
+func (suite *UpdaterTestSuite) SetupTest() {
+	// TODO: implement
+	suite.Fail("not implemented yet")
+}
+
+// TestUpdater is the main test function
+func (suite *UpdaterTestSuite) TestUpdater() {
+	// TODO: implement
+	suite.FailNow("not implemented yet")
+}
+
+// TestUpdaterTestSuite is the main entrypoint for UpdaterTestSuite
+func TestUpdaterTestSuite(t *testing.T) {
+	suite.Run(t, new(UpdaterTestSuite))
+}
