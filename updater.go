f76de46e34b10551bc89d9d4245b14afcdf9469a
Jules Mikeal <julesmichaelgiovanni@gmail.com>
2019-11-21 15:13:17 +0400

    - refact - cater for pointer to struct

--- updater.go
+++ updater.go
@@ -12,21 +12,15 @@ type Updater func(existing interface{}, values map[string]interface{}) (interfac
 
 // New is a factory function that given an instance of an object will generate an "Updater" function.
 func New(instance interface{}) (Updater, error) {
-	schema, err := createSchemaFromInstance(instance)
+	schema, err := schemaFromInstance(instance)
 	if err != nil {
 		return nil, err
 	}
 
 	return func(existing interface{}, values map[string]interface{}) (interface{}, error) {
-		typeOfExisting := reflect.TypeOf(existing)
-
-		if typeOfExisting.Kind() != reflect.Struct {
-			return nil, errors.New("existing object must be of type struct")
-		}
-
-		newElem := reflect.New(typeOfExisting).Elem()
-		if !newElem.CanInterface() {
-			return nil, errors.New("new element from existing object cannot be casted to interface")
+		newElem, err := newElementFromStruct(existing)
+		if err != nil {
+			return nil, err
 		}
 
 		for name, propname := range schema {
@@ -62,12 +56,17 @@ func updateField(name, propname string, values map[string]interface{}, existing 
 	}
 }
 
-// createSchemaFromInstance accepts an instance of an object
+// schemaFromInstance accepts an instance of an object
 // and returns map[fieldname]propname
-func createSchemaFromInstance(instance interface{}) (map[string]string, error) {
+func schemaFromInstance(instance interface{}) (map[string]string, error) {
 	schema := make(map[string]string)
 
 	valElem := reflect.ValueOf(instance)
+
+	// if pointer get value it points to instead
+	if valElem.Kind() == reflect.Ptr {
+		valElem = valElem.Elem()
+	}
 
 	if valElem.Kind() != reflect.Struct {
 		return nil, errors.New("instance must be of type struct")
@@ -83,5 +82,30 @@ func createSchemaFromInstance(instance interface{}) (map[string]string, error) {
 		schema[fieldname] = propname
 	}
 
+	if len(schema) == 0 {
+		return nil, errors.New("derived schema is empty")
+	}
+
 	return schema, nil
+}
+
+// newElementFromStruct creates a new element from existing struct
+func newElementFromStruct(existing interface{}) (*reflect.Value, error) {
+	typeOfExisting := reflect.TypeOf(existing)
+
+	// if pointer get value it points to instead
+	if typeOfExisting.Kind() == reflect.Ptr {
+		typeOfExisting = typeOfExisting.Elem()
+	}
+
+	if typeOfExisting.Kind() != reflect.Struct {
+		return nil, errors.New("existing object must be of type struct")
+	}
+
+	newElem := reflect.New(typeOfExisting).Elem()
+	if !newElem.CanInterface() {
+		return nil, errors.New("new element from existing object cannot be casted to interface")
+	}
+
+	return &newElem, nil
 }
